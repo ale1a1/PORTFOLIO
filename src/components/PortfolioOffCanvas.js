fef5209a8d0b78ee89917e515d82651296735838
ale1a1 <68277684+ale1a1@users.noreply.github.com>
2022-06-09 15:35:18 +0100

    ...

--- src/components/PortfolioOffCanvas.js
+++ src/components/PortfolioOffCanvas.js
@@ -35,7 +35,7 @@ const PortfolioOffCanvas = () => {
               target="_blank"
               rel="noreferrer"
             >
-              <i className="bi bi-globe2"> </i> Locum shifts tracker
+              <i className="bi bi-globe2"> </i> Locum Shifts Tracker
             </a>
             <a
               href="https://629e10686924e5432efb1de7--merry-gingersnap-1ee2b5.netlify.app/"
@@ -46,7 +46,7 @@ const PortfolioOffCanvas = () => {
               target="_blank"
               rel="noreferrer"
             >
-              <i className="bi bi-globe2"></i> Bankist
+              <i className="bi bi-globe2"></i> Bankist App
             </a>
             <a
               href="https://business-website-alw.herokuapp.com/"
@@ -70,6 +70,17 @@ const PortfolioOffCanvas = () => {
               rel="noreferrer"
             >
               <i className="bi bi-globe2"></i> Bankist Website
+            </a>
+            <a
+              href="https://funny-clafoutis-9d206c.netlify.app/"
+              className="
+              list-group-item list-group-item-danger list-group-item-action
+              mb-2
+            "
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="bi bi-globe2"> </i> Find Users App
             </a>
           </ul>
         </div>

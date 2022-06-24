28343ddd9d2dceb7ae394e5a68cfec9151756a75
ale1a1 <68277684+ale1a1@users.noreply.github.com>
2022-06-24 13:25:09 +0100

    ...

--- src/components/AboutAndContact.js
+++ src/components/AboutAndContact.js
@@ -10,19 +10,21 @@ const AboutAndContact = () => {
             <h5 className="card-title text-danger ">About</h5>
             <div className="card-text">
               <h3 className="text-white-50 ms-3">Alessandro Ladu</h3>
-              <ul className="list-unstyled ms-2 bulletPoint">                
+              <ul className="list-unstyled ms-2 bulletPoint">
                 <li>Freelance Web Developer</li>
                 <li>DVM</li>
                 <li>CEO at A.L.W. Media</li>
-                <li>Languages: HTML, JS, CSS, Sass</li>               
+                <li>Languages: HTML, JS, CSS</li>
               </ul>
             </div>
           </div>
         </div>
       </div>
-      <div className="col-md-6 mt-3 removeOnSmallScreens {
+      <div
+        className="col-md-6 mt-3 removeOnSmallScreens {
     display: none;
-  }">
+  }"
+      >
         <div className="collapse multi-collapse" id="contact">
           <div className="card card-body text-start shadow bg-dark-transparent">
             <h5 className="card-title text-danger ">Contact</h5>
@@ -31,20 +33,23 @@ const AboutAndContact = () => {
               <ul className="list-unstyled ms-5">
                 <li>
                   {" "}
-                  <i className="bi bi-house me-2 text-danger"></i> City:  Liverpool
+                  <i className="bi bi-house me-2 text-danger"></i> City:
+                  Liverpool
                 </li>
                 <li>
                   {" "}
-                  <i className="bi bi-signpost me-2 text-danger"></i> Street:  Smithdown Road
-                  
+                  <i className="bi bi-signpost me-2 text-danger"></i> Street:
+                  Smithdown Road
                 </li>
                 <li>
                   {" "}
-                  <i className="bi bi-telephone me-2 text-danger"></i> Phone:  +447481184619
+                  <i className="bi bi-telephone me-2 text-danger"></i> Phone:
+                  +447481184619
                 </li>
                 <li>
                   {" "}
-                  <i className="bi bi-envelope me-2 text-danger"></i> Email: ale1a184@gmail.com
+                  <i className="bi bi-envelope me-2 text-danger"></i> Email:
+                  ale1a184@gmail.com
                 </li>
               </ul>
             </div>
--- src/components/PortfolioOffCanvas.js
+++ src/components/PortfolioOffCanvas.js
@@ -20,12 +20,13 @@ const PortfolioOffCanvas = () => {
         </div>
         <div className="offcanvas-body">
           <p>
-            Have a look at my work ! <br></br>
+            Have a look at my work 😊<br></br>
             Some projects are my own work and some are created coding along the
             tutors on the online courses I followed, using React, Html,
-            Javascript, Css, and or Bootstrap 5. <br></br>
+            Javascript, Css and Bootstrap 5 😊. <br></br>
           </p>
           <ul className="list-group">
+            <h6 className="mt-4 mb-2 portfolioHeader">RESPONSIVE DESIGN</h6>
             <a
               href="https://locum-shifts.herokuapp.com/"
               className="
@@ -37,17 +38,7 @@ const PortfolioOffCanvas = () => {
             >
               <i className="bi bi-globe2"> </i> Locum Shifts Tracker
             </a>
-            <a
-              href="https://merry-gingersnap-1ee2b5.netlify.app/"
-              className="
-              list-group-item list-group-item-danger list-group-item-action
-              mb-2
-            "
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="bi bi-globe2"></i> Bankist App
-            </a>
+
             <a
               href="https://business-website-alw.herokuapp.com/"
               className="
@@ -61,17 +52,6 @@ const PortfolioOffCanvas = () => {
             </a>
 
             <a
-              href="https://bankist-website-jonas-schmedtm.herokuapp.com/"
-              className="
-              list-group-item list-group-item-danger list-group-item-action
-              mb-2
-            "
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="bi bi-globe2"></i> Bankist Website
-            </a>
-            <a
               href="https://funny-clafoutis-9d206c.netlify.app/"
               className="
               list-group-item list-group-item-danger list-group-item-action
@@ -81,6 +61,29 @@ const PortfolioOffCanvas = () => {
               rel="noreferrer"
             >
               <i className="bi bi-globe2"> </i> Find Users App
+            </a>
+            <h6 className="mt-4 mb-2 portfolioHeader">NON RESPONSIVE DESIGN</h6>
+            <a
+              href="https://merry-gingersnap-1ee2b5.netlify.app/"
+              className="
+              list-group-item list-group-item-danger list-group-item-action
+              mb-2
+            "
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="bi bi-globe2"></i> Bankist App
+            </a>
+            <a
+              href="https://bankist-website-jonas-schmedtm.herokuapp.com/"
+              className="
+              list-group-item list-group-item-danger list-group-item-action
+              mb-2
+            "
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="bi bi-globe2"></i> Bankist Website
             </a>
           </ul>
         </div>

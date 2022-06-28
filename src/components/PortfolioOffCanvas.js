6fdc896e4ebb5d77e89b3ae14e06d0082c57dd6b
ale1a1 <68277684+ale1a1@users.noreply.github.com>
2022-06-28 16:54:31 +0100

    ...

--- src/components/PortfolioOffCanvas.js
+++ src/components/PortfolioOffCanvas.js
@@ -52,6 +52,18 @@ const PortfolioOffCanvas = () => {
             </a>
 
             <a
+              href="https://62bb23cee58d5032c7aef0ea--adorable-sunburst-74416f.netlify.app/"
+              className="
+              list-group-item list-group-item-danger list-group-item-action
+              mb-2
+            "
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="bi bi-globe2"></i> uHost Website
+            </a>
+
+            <a
               href="https://funny-clafoutis-9d206c.netlify.app/"
               className="
               list-group-item list-group-item-danger list-group-item-action

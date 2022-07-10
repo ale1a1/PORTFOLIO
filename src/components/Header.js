bed55041ce80114e105d5d429c87d68207f6f22b
ale1a1 <68277684+ale1a1@users.noreply.github.com>
2022-07-10 19:10:19 +0100

    ...

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,22 +6,24 @@ const Header = () => {
   return (
     <Fragment>
       <header className="container fixed-top text-end mt-5">
-        <h1 className="display-1 cssSmallScreens">
+        <h1 className="display-1 cssSmallScreens mt-3">
           Hi. I am <span className="badge bg-secondary">Alex</span>
         </h1>
-        <h2 className="display-3 cssSmallScreens cssSmallScreensSmallerFont">Web Developer</h2>
+        <h2 className="display-3 cssSmallScreens cssSmallScreensSmallerFont mt-3">
+          Web Developer
+        </h2>
         <div className="btn-group margin removeOnSmallScreens">
           <button
-            className="btn btn-outline-danger text-uppercase bg-dark2-transparent cssSmallScreens"
+            className="btn btn-outline-danger text-uppercase bg-dark2-transparent cssSmallScreens mt-5 me-2 round-corners"
             type="button"
             data-bs-toggle="offcanvas"
             data-bs-target="#portfolio"
             aria-controls="portfolio"
           >
-            my portfolio
+            portfolio
           </button>
           <button
-            className="btn btn-outline-warning bg-dark2-transparent cssSmallScreens"
+            className="btn btn-outline-warning bg-dark2-transparent cssSmallScreens mt-5 round-corners"
             type="button"
             data-bs-toggle="offcanvas"
             data-bs-target="#email"

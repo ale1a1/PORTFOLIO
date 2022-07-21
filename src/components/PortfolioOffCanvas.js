74b6cd18fab4cd026e17892354ac8b6bfbf24a45
ale1a1 <68277684+ale1a1@users.noreply.github.com>
2022-07-21 12:22:01 +0100

    ...

--- src/components/PortfolioOffCanvas.js
+++ src/components/PortfolioOffCanvas.js
@@ -19,89 +19,92 @@ const PortfolioOffCanvas = () => {
           ></button>
         </div>
         <div className="offcanvas-body">
-          <p className="mb-4 mt-3">
-            Have a look at my work 😊<br></br>
-            Some projects are my own work and some are created coding along the
-            tutors on the online courses I followed, using React, Html,
-            Javascript, Css and Bootstrap 5 😊 <br></br>
-          </p>
-          <ul className="list-group">
-            <a
-              href="https://omnifood-alw.netlify.app/"
-              className="
+          <div className="card card-body text-start shadow offCanvasPortfolioInfoCard">
+            <p className="mb-4 mt-3">
+              <span className="h5 mb-5"> Have a look at my work</span> 😊
+              <br></br> <br></br>
+              Some projects are my own work and some are created coding along
+              the tutors on the online courses I followed, using React, Html,
+              Javascript, Css and Bootstrap 5 😊 <br></br>
+            </p>
+            <ul className="list-group">
+              <a
+                href="https://omnifood-alw.netlify.app/"
+                className="
               list-group-item list-group-item-danger list-group-item-action
               mb-3 mt-4 round-corners
             "
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="bi bi-globe2"> </i>{" "}
-              <span className="ms-3">Omnifood website</span>
-            </a>
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="bi bi-globe2"> </i>{" "}
+                <span className="ms-3">Omnifood website</span>
+              </a>
 
-            <a
-              href="https://business-directory-alw.netlify.app/"
-              className="
+              <a
+                href="https://business-directory-alw.netlify.app/"
+                className="
               list-group-item list-group-item-danger list-group-item-action
               mb-3 round-corners
             "
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="bi bi-globe2"></i>
-              <span className="ms-3">Business Directory website</span>
-            </a>
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="bi bi-globe2"></i>
+                <span className="ms-3">Business Directory website</span>
+              </a>
 
-            <a
-              href="https://uhost-alw.netlify.app/"
-              className="
+              <a
+                href="https://uhost-alw.netlify.app/"
+                className="
               list-group-item list-group-item-danger list-group-item-action
               mb-3 round-corners
             "
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="bi bi-globe2"></i>
-              <span className="ms-3">uHost website</span>
-            </a>
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="bi bi-globe2"></i>
+                <span className="ms-3">uHost website</span>
+              </a>
 
-            <a
-              href="https://find-users-app.netlify.app/"
-              className="
+              <a
+                href="https://find-users-app.netlify.app/"
+                className="
               list-group-item list-group-item-danger list-group-item-action
               mb-3 round-corners
             "
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="bi bi-globe2"> </i>
-              <span className="ms-3">Find Users app</span>
-            </a>
-            <a
-              href="https://lisbon-chairs-shop.netlify.app/"
-              className="
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="bi bi-globe2"> </i>
+                <span className="ms-3">Find Users app</span>
+              </a>
+              <a
+                href="https://lisbon-chairs-shop.netlify.app/"
+                className="
               list-group-item list-group-item-danger list-group-item-action
               mb-3 round-corners
             "
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="bi bi-globe2"></i>
-              <span className="ms-3">Lisbon Chairs Shop website</span>
-            </a>
-            <a
-              href="https://bankist-alw.netlify.app/"
-              className="
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="bi bi-globe2"></i>
+                <span className="ms-3">Lisbon Chairs Shop website</span>
+              </a>
+              <a
+                href="https://bankist-alw.netlify.app/"
+                className="
               list-group-item list-group-item-danger list-group-item-action
               mb-3 round-corners
             "
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="bi bi-globe2"></i>
-              <span className="ms-3">Bankist website</span>
-            </a>
-          </ul>
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="bi bi-globe2"></i>
+                <span className="ms-3">Bankist website</span>
+              </a>
+            </ul>
+          </div>
         </div>
       </div>
     </Fragment>

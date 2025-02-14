cc78bd437cc604d3eca3f5fdaf08572b84da5135
ale1a1 <68277684+ale1a1@users.noreply.github.com>
2025-02-14 14:07:29 +0000

    fix an issue with the portfolio button on medium screens

--- src/components/PortfolioOffCanvas.js
+++ src/components/PortfolioOffCanvas.js
@@ -50,6 +50,15 @@ const PortfolioOffCanvas = () => {
                 className="list-group-item list-group-item-danger list-group-item-action mb-3 mb-md-4 rounded"
                 target="_blank"
                 rel="noreferrer"
+                onClick={() => handleProjectClick("Quiz Game")}
+              >
+                <i className="bi bi-globe2"></i>
+                <span className="ms-3">Su Sinnu</span>
+              </div>
+              <div
+                className="list-group-item list-group-item-danger list-group-item-action mb-3 mb-md-4 rounded"
+                target="_blank"
+                rel="noreferrer"
                 onClick={() => handleProjectClick("Find Users app")}
               >
                 <i className="bi bi-globe2"> </i>
